44c0c9a7c358505b75a955fafd1a378e37fbc75c
JonyHana <60459977+JonyHana@users.noreply.github.com>
2022-12-16 14:07:14 -0800

    more TypeBox revision

--- frontend/src/components/TypeBox.tsx
+++ frontend/src/components/TypeBox.tsx
@@ -8,15 +8,16 @@ const STATUS_COLORS: { [index: number]: string } = {
   0: 'text-neutral-400',
   1: 'text-lime-400',
   2: 'text-red-400',
+  4: 'text-neutral-300',
 }
 
 const TypeBox = ({ sentence }: TypeBoxProp) => {
   const [words, setWords] = useState<string[]>([]);
   const [inputText, setInputText] = useState<string>('');
+  const [inputDisabled, setInputDisabled] = useState<boolean>(false);
 
   //const [cursorIndex, setCursorIndex] = useState<number>(0); // currentWordIndex + len
   const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
-  //const [cursorCorrectIndex, setCursorCorrectIndex] = useState<number>(0);
   // 0 = incomplete, 1 = correct, 2 = incorrect
   // Letters will be compared to position index in sentence prop.
   const [lettersStatus, setLettersStatus] = useState<number[]>([]);
@@ -24,15 +25,15 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
   const [highlightIndex, setHighlightIndex] = useState<number>(0);
 
   useEffect(() => {
-    let wordsArr: string[] = sentence.split(' ');
-    let setWordsArr: string[] = wordsArr.map((word, key) => {
+    let wordsArr = sentence.split(' ');
+    let setWordsArr = wordsArr.map((word, key) => {
       //console.log(word, key, wordsArr.length);
       return (key !== wordsArr.length - 1) ? word + '\u00A0' : word;
     })
     //console.log(setWordsArr);
     
     let ltrsStatInit: number[] = [];
-    for (let i: number = 0; i < sentence.length; i++) {
+    for (let i = 0; i < sentence.length; i++) {
       ltrsStatInit.push(0);
     }
     
@@ -41,46 +42,54 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
   }, []);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let inTxt: string = e.target.value;
-    let len: number = inTxt.length;
+    const inTxt = e.target.value;
+    const inLen = inTxt.length;
+    const cursor = currentWordIndex + inLen;
 
-    if (len > sentence.length) {
+    if (cursor > sentence.length) {
       return false;
     }
 
     setInputText(inTxt);
+    //setCursorIndex(cursor);
 
-    let cursor = currentWordIndex + len;
-    //setCursorIndex(len);
-
-    //console.log(cursorCorrectIndex, len);
-    
-    setLettersStatus(
-      lettersStatus.map((status, index, arr) => {
-        if (index >= cursor) return 0;
-        if (index < currentWordIndex) return 0; // NOTE: temporarily 0, make this 1 later.
-        
-        let letter: string = sentence[index];
-        let letter2: string = inTxt[index];
-
-        return (letter === letter2) ? 1 : 2;
-      })
-    );
-    
-    // (RESUME)
-    // TODO: check if word matches. also, this seems like it'd be good for setting letter status.
-    let wordFill = sentence.substring(currentWordIndex, currentWordIndex + len);
-    console.log(inTxt, wordFill, inTxt === wordFill);
-    
+    let wordFill = sentence.substring(currentWordIndex, cursor);
+    //console.log(inTxt, wordFill, inTxt === wordFill, cursor, sentence.length);
     if (inTxt === wordFill) {
-      if (len < sentence.length - 1) {
+      if (cursor === sentence.length) {
         console.log('game end');
+        setInputDisabled(true);
+        setInputText('');
+        setCurrentWordIndex(cursor + 1);
       }
-      else if (inTxt[len] === ' ') {
-        console.log('word?');
+      else if (inTxt[inLen - 1] === ' ') {
+        console.log('word complete');
+        setInputText('');
+        setCurrentWordIndex(cursor);
       }
     }
+    else {
+      console.log('wrong');
+    }
   }
+  
+  useEffect(() => {
+    if (lettersStatus.length === 0) return; // To make sure lettersStatus has been initialized.
+
+    let cursor = currentWordIndex + (inputText.length);
+    let wordFill = sentence.substring(currentWordIndex, cursor);
+
+    setLettersStatus(
+      lettersStatus.map((status, index, arr) => {
+        //console.log(status, index, arr);
+        
+        if (index >= cursor) return 0;
+        if (index < currentWordIndex) return 4;
+        
+        return (inputText === wordFill) ? 1 : 2;
+      })
+    );
+  }, [inputText]);
   
   return (
     <div className="w-5/12">
@@ -90,13 +99,17 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
             let slength = 0;
             return words.map((word, wkey) => {
               slength += word.length;
-              let letters: string[] = [...word];
-              return (<span className='word inline-block' key={wkey}>
-                {letters.map((letter, lkey) => {
-                  let statusColor = STATUS_COLORS[lettersStatus[(slength - word.length) + lkey]];
-                  return <span className={`letter ${statusColor}`} key={lkey}>{letter}</span>
-                })}
-              </span>);
+              let letters = [...word];
+              return (
+                <span className='word inline-block' key={wkey}>
+                  {letters.map((letter, lkey) => {
+                    let statusColor = STATUS_COLORS[lettersStatus[(slength - word.length) + lkey]];
+                    return (
+                      <span className={`letter ${statusColor}`} key={lkey}>{letter}</span>
+                    );
+                  })}
+                </span>
+              );
             })
           })()
         }
@@ -105,6 +118,7 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
         type='text'
         /* className='opacity-0 cursor-default' */
         value={inputText}
+        disabled={inputDisabled}
         onChange={handleInputChange}
         onCopy={(e) => { e.preventDefault(); return false; }}
         onPaste={(e) => { e.preventDefault(); return false; }}
