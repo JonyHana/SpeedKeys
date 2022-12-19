49e527522719bceaec3187052f9ad1cb8fd832c4
JonyHana <60459977+JonyHana@users.noreply.github.com>
2022-12-18 16:16:03 -0800

    TypeBox revised, good to go

--- frontend/src/components/TypeBox.tsx
+++ frontend/src/components/TypeBox.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState, useRef, MouseEvent } from "react";
 
 type TypeBoxProp = {
   sentence: string;
@@ -16,19 +16,22 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
   const [inputText, setInputText] = useState<string>('');
   const [inputDisabled, setInputDisabled] = useState<boolean>(false);
 
-  //const [cursorIndex, setCursorIndex] = useState<number>(0); // currentWordIndex + len
+  const inputBoxRef = useRef<HTMLInputElement>(null);
+  
+  const [baseCursorIndex, setBaseCursorIndex] = useState<number>(0);
   const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
   // 0 = incomplete, 1 = correct, 2 = incorrect
   // Letters will be compared to position index in sentence prop.
   const [lettersStatus, setLettersStatus] = useState<number[]>([]);
   // Highlighting from left to cursorIndex.
-  const [highlightIndex, setHighlightIndex] = useState<number>(0);
+  //const [highlightIndex, setHighlightIndex] = useState<number>(0);
 
+  // TypeBox initialization
   useEffect(() => {
     let wordsArr = sentence.split(' ');
     let setWordsArr = wordsArr.map((word, key) => {
       //console.log(word, key, wordsArr.length);
-      return (key !== wordsArr.length - 1) ? word + '\u00A0' : word;
+      return (key !== wordsArr.length - 1) ? word + ' ' : word;
     })
     //console.log(setWordsArr);
     
@@ -40,59 +43,81 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
     setLettersStatus(ltrsStatInit);
     setWords(setWordsArr);
   }, []);
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inTxt = e.target.value;
-    const inLen = inTxt.length;
-    const cursor = currentWordIndex + inLen;
-
-    if (cursor > sentence.length) {
-      return false;
-    }
-
-    setInputText(inTxt);
-    //setCursorIndex(cursor);
-
-    let wordFill = sentence.substring(currentWordIndex, cursor);
-    //console.log(inTxt, wordFill, inTxt === wordFill, cursor, sentence.length);
-    if (inTxt === wordFill) {
-      if (cursor === sentence.length) {
-        console.log('game end');
-        setInputDisabled(true);
-        setInputText('');
-        setCurrentWordIndex(cursor + 1);
-      }
-      else if (inTxt[inLen - 1] === ' ') {
-        console.log('word complete');
-        setInputText('');
-        setCurrentWordIndex(cursor);
-      }
-    }
-    else {
-      console.log('wrong');
-    }
-  }
-  
+ 
+  // Changing letter status
   useEffect(() => {
-    if (lettersStatus.length === 0) return; // To make sure lettersStatus has been initialized.
+    // To make sure lettersStatus has been initialized.
+    if (lettersStatus.length === 0) return;
 
-    let cursor = currentWordIndex + (inputText.length);
-    let wordFill = sentence.substring(currentWordIndex, cursor);
+    let localCursorIndex = inputText.length;
+    let wordFill = words[currentWordIndex].substring(0, localCursorIndex);
 
     setLettersStatus(
       lettersStatus.map((status, index, arr) => {
         //console.log(status, index, arr);
         
-        if (index >= cursor) return 0;
-        if (index < currentWordIndex) return 4;
+        if (index >= baseCursorIndex + localCursorIndex) return 0;
+        if (index < baseCursorIndex) return 4;
         
         return (inputText === wordFill) ? 1 : 2;
       })
     );
   }, [inputText]);
   
+  // When the current word is completed
+  useEffect(() => {
+    // To make sure not to execute on TypeBox initialization.
+    if (baseCursorIndex === 0) return;
+
+    const localCursorIndex = inputText.length;
+    const globalCursorIndex = baseCursorIndex + localCursorIndex;
+    
+    if (globalCursorIndex === sentence.length) {
+      //console.log('sentence complete');
+      setInputDisabled(true);
+      setInputText('');
+      setBaseCursorIndex(globalCursorIndex + 1);
+    }
+    else {
+      //console.log('word complete');
+      setInputText('');
+      setBaseCursorIndex(globalCursorIndex);
+    }
+    
+    //console.log('setCurrentWordIndex(currentWordIndex + 1)');
+    setCurrentWordIndex(currentWordIndex + 1);
+  }, [baseCursorIndex]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inTxt = e.target.value;
+    const localCursorIndex = inTxt.length;
+    const currWord = words[currentWordIndex];
+    
+    if (localCursorIndex > currWord.length) {
+      return false;
+    }
+
+    setInputText(inTxt);
+
+    const wordFill = currWord.substring(0, localCursorIndex);
+    if (inTxt === wordFill && localCursorIndex === currWord.length) {
+      setInputText('');
+      setBaseCursorIndex(baseCursorIndex + localCursorIndex);
+    }
+    /*else {
+      console.log('wrong');
+    }*/
+  }
+
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (inputBoxRef && inputBoxRef.current)
+      inputBoxRef.current.focus();
+    else
+      console.log('[E] no input found');
+  }
+  
   return (
-    <div className="w-5/12">
+    <div className="w-5/12" onClick={handleClick}>
       <div className="bg-neutral-900 w-auto text-2xl p-3 rounded-lg">
         {words &&
           (() => {
@@ -104,8 +129,9 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
                 <span className='word inline-block' key={wkey}>
                   {letters.map((letter, lkey) => {
                     let statusColor = STATUS_COLORS[lettersStatus[(slength - word.length) + lkey]];
+                    let ltr = (letter === ' ' ? '\u00A0' : letter);
                     return (
-                      <span className={`letter ${statusColor}`} key={lkey}>{letter}</span>
+                      <span className={`letter ${statusColor}`} key={lkey}>{ltr}</span>
                     );
                   })}
                 </span>
@@ -116,9 +142,10 @@ const TypeBox = ({ sentence }: TypeBoxProp) => {
       </div>
       <input
         type='text'
-        /* className='opacity-0 cursor-default' */
+        className='opacity-0 cursor-default'
         value={inputText}
         disabled={inputDisabled}
+        ref={inputBoxRef}
         onChange={handleInputChange}
         onCopy={(e) => { e.preventDefault(); return false; }}
         onPaste={(e) => { e.preventDefault(); return false; }}
